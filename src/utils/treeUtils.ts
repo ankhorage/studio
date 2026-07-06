32eafee51915fdde4dd47321ca6e4c321dcbf8e6
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-07-07 00:03:27 +0200

    Add Studio tree utility exports

---
+++ src/utils/treeUtils.ts
@@ -0,0 +1,17 @@
+import {
+  addNodeToTree,
+  cloneWithNewIds,
+  findNodeById,
+  moveNodeInTree,
+  removeNodeFromTree,
+  updateNodeInTree,
+} from '../index';
+
+export {
+  addNodeToTree,
+  cloneWithNewIds,
+  findNodeById,
+  moveNodeInTree,
+  removeNodeFromTree,
+  updateNodeInTree,
+};
