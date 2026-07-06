f898b62a84d500a149ce431ec81a34cd4c0ff694
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-07-06 23:49:18 +0200

    Remove redundant Knip configuration hints

--- knip.config.ts
+++ knip.config.ts
@@ -4,11 +4,11 @@ export default createKnipConfig({
   workspaces: {
     '.': {
       entry: ['src/index.ts', 'src/app/index.ts'],
-      project: ['src/**/*.ts', 'src/**/*.tsx', 'knip.config.ts', 'paradox.config.ts'],
+      project: ['src/**/*.ts', 'src/**/*.tsx', 'paradox.config.ts'],
       ignoreFiles: ['paradox.config.ts'],
     },
     'apps/studio': {
-      entry: ['app.config.ts', 'babel.config.js', 'index.js', 'src/app/_layout.tsx', 'src/app/index.tsx'],
+      entry: ['babel.config.js'],
       project: ['**/*.js', '**/*.ts', '**/*.tsx'],
       ignoreDependencies: ['babel-preset-expo', 'babel-plugin-module-resolver'],
     },
