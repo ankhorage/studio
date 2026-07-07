fe14913797941f65270e7f517d3ed3206e92607c
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-07-07 12:42:50 +0200

    Add root entry to Knip

--- knip.config.ts
+++ knip.config.ts
@@ -4,6 +4,7 @@ export default createKnipConfig({
   workspaces: {
     '.': {
       entry: [
+        'src/root.ts',
         'src/index.ts',
         'src/app/index.ts',
         'src/cli/index.ts',
