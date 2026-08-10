6f4e6c8d658f2f7c82e856e4a1b1c8785ed087d5
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-08-11 01:16:01 +0200

    feat: expose binding authoring model

---
+++ src/bindingAuthoringModel.ts
@@ -0,0 +1,6 @@
+export * from './bindingAuthoringContracts';
+export * from './bindingDiagnosticsModel';
+export * from './bindingMetadataModel';
+export * from './bindingMutationModel';
+export * from './bindingOperationModel';
+export * from './bindingSchemaModel';
