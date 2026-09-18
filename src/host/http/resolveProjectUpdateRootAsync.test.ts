e5261ca8219d48d70527ab46e136284714ef7003
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-18 16:20:59 +0200

    style: sort project update discovery imports

--- src/host/http/resolveProjectUpdateRootAsync.test.ts
+++ src/host/http/resolveProjectUpdateRootAsync.test.ts
@@ -1,7 +1,8 @@
-import { expect, test } from 'bun:test';
 import { mkdir, mkdtemp, rm, writeFile } from 'node:fs/promises';
 import { tmpdir } from 'node:os';
 import path from 'node:path';
+
+import { expect, test } from 'bun:test';
 
 import { resolveProjectUpdateRootAsync } from './resolveProjectUpdateRootAsync';
 
