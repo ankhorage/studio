477700d7a2df92c2a20f56026ba378721901f531
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-18 08:54:29 +0200

    style(manifest): format theme assertion

--- src/manifestState.test.ts
+++ src/manifestState.test.ts
@@ -731,9 +731,9 @@ describe('manifestState', () => {
 
     expect(oauth.navigator.type).toBe('drawer');
     expect(oauth.navigator.initialRouteName).toBe('about');
-    expect(
-      Object.values(oauth.themes).find((theme) => theme.id === 'theme-2')?.name,
-    ).toBe('Updated Theme');
+    expect(Object.values(oauth.themes).find((theme) => theme.id === 'theme-2')?.name).toBe(
+      'Updated Theme',
+    );
     expect(oauth.infra.environments.local.auth?.oauth?.providers).toHaveLength(1);
   });
 });
